c708d910eab955f67f8dd3aa7db5726c330ceb00
RDSM243 <didicomendes13@gmail.com>
2025-07-14 12:53:40 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# TODO: Ler os parametros da linha de comando e atribuir para as variaveis nsets, bsize e assoc
+nsets = 0
+bsize = 0
+assoc = 0
+
+# Criar uma estrutura de dados para armazenar os tags e os bits de validade.
+cache_tag = []
+cache_val = []
+
+n_bits_offset = 0
+n_bits_indice = 0
+
+# Percorre todos os endereços do arquivo de entrada.
+while(True):
+    print("teste")
